ca8d55469849bf7c51b1478a1c84c4a74223c006
Batch42 <stproctor1@smcm.edu>
2015-11-02 17:43:53 -0500

    First edition of the project

---
+++ Assets/Bludger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bludger : MonoBehaviour {
+	public GameObject Player;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 atPlayer = Vector2.MoveTowards(new Vector2(0,0),Player.transform.position- transform.position,1)*42;
+		Debug.Log (atPlayer);
+		GetComponent<Rigidbody2D>().AddForce(atPlayer);
+	}
+}
---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// FixedUpdate is called once per tick
+	void FixedUpdate () {
+		float h = Input.GetAxis ("Horizontal")/10;
+		float v = Input.GetAxis ("Vertical")/10;
+		transform.Translate(new Vector3 (h, v, 0));
+	}
+}
